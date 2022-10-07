8761aa76ead6ef024c63babac8006a8030737174
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2022-10-07 14:26:36 -0400

    simple state

--- src/components/ItemFormContainer.js
+++ src/components/ItemFormContainer.js
@@ -1,17 +1,40 @@
 import React, { useState } from "react"
 
 const ItemFormContainer = (props) => {
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
 
+  const handleNameChange = (event) => {
+    // set the name input value into state
+    // debugger
+    setName(event.currentTarget.value)
+  }
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.currentTarget.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = {
+      itemName: name,
+      itemDescription: description
+    }
+    // debugger
+    props.addItem(formData)
+  }
 
   return (
     <div>
-      <form >
+      <form onSubmit={handleSubmit}>
         <label htmlFor="itemName">Name:
           <input
             type="text"
             id="itemName"
             name="itemName"
-            />
+            onChange={handleNameChange}
+            value={name}
+          />
         </label>
 
         <label htmlFor="itemDescription">Description:
@@ -19,6 +42,8 @@ const ItemFormContainer = (props) => {
             type="text"
             id="itemDescription"
             name="itemDescription"
+            value={description}
+            onChange={handleDescriptionChange}
           />
         </label>
 
